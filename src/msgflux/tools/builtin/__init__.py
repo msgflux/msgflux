822caadb39fba6ae3f4e3a2c8d84ea8408e8005d
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2026-07-04 11:07:19 -0300

    Add agent tool bucket (#136)

--- src/msgflux/tools/builtin/__init__.py
+++ src/msgflux/tools/builtin/__init__.py
@@ -1,5 +1,6 @@
 """Built-in agent tools ready for use out of the box."""
 
+from msgflux.tools.builtin.agent import AgentTool
 from msgflux.tools.builtin.agent_skills import ActivateSkill, SkillSearch
 from msgflux.tools.builtin.tool_search import ToolSearchTool
 from msgflux.tools.builtin.weather import Weather
@@ -8,6 +9,7 @@ from msgflux.tools.builtin.web_search import WebSearch
 
 __all__ = [
     "ActivateSkill",
+    "AgentTool",
     "SkillSearch",
     "ToolSearchTool",
     "Weather",
