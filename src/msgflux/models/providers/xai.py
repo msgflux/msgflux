db36949451c7ba7b5c7f804ba593e3093b05a696
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2026-09-14 21:05:10 -0300

    refactor: share session headers from msgflux.models.session

--- src/msgflux/models/providers/xai.py
+++ src/msgflux/models/providers/xai.py
@@ -11,12 +11,12 @@ from msgflux.models.openai_compatible import (
     OpenAICompatibleChatCompletion,
     OpenAIResponsesAPI,
 )
-from msgflux.models.providers._session import USER_AGENT, merge_session_headers
 from msgflux.models.reasoning import (
     OpenAICompatibleReasoningCodec,
     OpenAIResponsesReasoningCodec,
 )
 from msgflux.models.registry import register_model
+from msgflux.models.session import USER_AGENT, merge_session_headers
 from msgflux.runtime.context import get_thread_id
 
 
